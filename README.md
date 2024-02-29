3260596cc4668bd5386c322a2a452d421e41faad
zeeoneofficial <videorandom725@gmail.com>
2024-02-29 12:39:06 +0800

    Push Kontak

---
+++ README.md
@@ -0,0 +1,1 @@
+# BOT PUSH KONTAK
---
+++ settings.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+global.owner = ["62887435047326"]
+global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
+global.pairingNumber = ""
