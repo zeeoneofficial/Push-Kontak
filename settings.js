698d165336523ba91ab025cee2b3f3407540f26a
Arisjb <158495512+Arisjb@users.noreply.github.com>
2024-03-26 22:28:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
 global.owner = ["62887435047326"]
-global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.push_kontak_delay = 4000 //5 detik, 1000 = 1 detik
+global.pairingNumber = "6285810151395"
